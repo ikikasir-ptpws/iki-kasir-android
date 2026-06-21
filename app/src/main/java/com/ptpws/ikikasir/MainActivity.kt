27d12411f19ebeba87f7f263ab95afb0c5f81044
Ikhlas Sahputra <ikhlasamalillah568@gmail.com>
2026-06-21 15:37:48 +0700

    replace UpdateStokScreen with KasirScreen in MainActivity

--- app/src/main/java/com/ptpws/ikikasir/MainActivity.kt
+++ app/src/main/java/com/ptpws/ikikasir/MainActivity.kt
@@ -39,7 +39,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             IKIKASIRTheme {
 //                AppNavHost()
-                UpdateStokScreen()
+                KasirScreen()
             }
         }
     }
