f3477d2b3d9842d9c648c56d833234629a310684
Ikhlas Sahputra <ikhlasamalillah568@gmail.com>
2026-06-23 14:19:10 +0700

    replace PengaturanDashboardScreen with ProfilScreen in MainActivity for user profile management

--- app/src/main/java/com/ptpws/ikikasir/MainActivity.kt
+++ app/src/main/java/com/ptpws/ikikasir/MainActivity.kt
@@ -28,6 +28,7 @@ import com.ptpws.ikikasir.screens.manajemenstok.ManajemenStokScreen
 import com.ptpws.ikikasir.screens.manajemenstok.component.UpdateStokScreen
 import com.ptpws.ikikasir.screens.navigation.AppNavHost
 import com.ptpws.ikikasir.screens.pengaturan.PengaturanDashboardScreen
+import com.ptpws.ikikasir.screens.pengaturan.ProfilScreen
 import com.ptpws.ikikasir.screens.penjualan.DetailTransaksiScreen
 import com.ptpws.ikikasir.screens.penjualan.LaporanPenjualanScreen
 import com.ptpws.ikikasir.screens.penjualan.RiwayatTransaksiScreen
@@ -44,7 +45,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             IKIKASIRTheme {
 //                AppNavHost()
-                PengaturanDashboardScreen()
+                ProfilScreen()
             }
         }
     }
