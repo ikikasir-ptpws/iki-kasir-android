bb8b3189c8cb72f59839567de4ec74c94490cd62
Ikhlas Sahputra <ikhlasamalillah568@gmail.com>
2026-09-02 13:27:14 +0700

    feat: update settings.gradle.kts to include Hilt plugin resolution and additional Maven repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,11 @@
 pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.google.dagger.hilt.android") {
+                useModule("com.google.dagger:hilt-android-gradle-plugin:${requested.version}")
+            }
+        }
+    }
     repositories {
         google {
             content {
@@ -8,6 +15,8 @@ pluginManagement {
             }
         }
         mavenCentral()
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://repo1.maven.org/maven2/") }
         gradlePluginPortal()
     }
 }
@@ -16,9 +25,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://repo1.maven.org/maven2/") }
     }
 }
 
 rootProject.name = "IKIKASIR"
 include(":app")
- 
