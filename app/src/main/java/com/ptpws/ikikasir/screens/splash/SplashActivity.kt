9e8338c17b24ae1211cc3f455fba779fa97d3a0e
Ikhlas Sahputra <ikhlasamalillah568@gmail.com>
2026-09-02 13:29:00 +0700

    feat: update SplashActivity to check user authentication status before navigation

--- app/src/main/java/com/ptpws/ikikasir/screens/splash/SplashActivity.kt
+++ app/src/main/java/com/ptpws/ikikasir/screens/splash/SplashActivity.kt
@@ -6,7 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.LaunchedEffect
-import com.ptpws.ikikasir.screens.auth.AuthActivity
+import com.ptpws.ikikasir.feature.auth.presentation.screen.AuthActivity
 import com.ptpws.ikikasir.screens.splash.ui.theme.IKIKASIRTheme
 import kotlinx.coroutines.delay
 
@@ -20,7 +20,16 @@ class SplashActivity : ComponentActivity() {
                 
                 LaunchedEffect(key1 = true) {
                     delay(2000L) // Wait for 2 seconds
-                    startActivity(Intent(this@SplashActivity, AuthActivity::class.java))
+                    val auth = com.google.firebase.auth.FirebaseAuth.getInstance()
+                    
+                    android.util.Log.d("AuthStatus", "Pengecekan Sesi... Current User Email: ${auth.currentUser?.email ?: "Tidak ada (Belum Login)"}")
+                    
+                    if (auth.currentUser != null) {
+                        android.util.Log.d("AuthStatus", "Mengarahkan ke MainActivity...")
+                        startActivity(Intent(this@SplashActivity, com.ptpws.ikikasir.MainActivity::class.java))
+                    } else {
+                        startActivity(Intent(this@SplashActivity, AuthActivity::class.java))
+                    }
                     finish()
                 }
             }
