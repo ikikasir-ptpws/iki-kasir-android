abf1568247a0e1b961c4ef149f3cc8258a9d7fc0
Ikhlas Sahputra <ikhlasamalillah568@gmail.com>
2026-09-09 19:43:39 +0700

    feat: add ProdukEntity to AppDatabase and increment version to 2

--- app/src/main/java/com/ptpws/ikikasir/core/database/AppDatabase.kt
+++ app/src/main/java/com/ptpws/ikikasir/core/database/AppDatabase.kt
@@ -4,16 +4,20 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.ptpws.ikikasir.feature.kategori.data.local.dao.KategoriDao
 import com.ptpws.ikikasir.feature.kategori.data.local.entity.KategoriEntity
+import com.ptpws.ikikasir.feature.produk.data.local.dao.ProdukDao
+import com.ptpws.ikikasir.feature.produk.data.local.entity.ProdukEntity
 
 @Database(
     entities = [
-        KategoriEntity::class
+        KategoriEntity::class,
+        ProdukEntity::class
     ],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract val kategoriDao: KategoriDao
+    abstract val produkDao: ProdukDao
 
     companion object {
         const val DATABASE_NAME = "ikikasir_db"
