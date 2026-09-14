852cd517bbe885666dfb33f253a144442ab6e43f
Ikhlas Sahputra <ikhlasamalillah568@gmail.com>
2026-09-14 14:45:18 +0700

    feat: add DetailProdukState data class for managing product detail state

---
+++ app/src/main/java/com/ptpws/ikikasir/feature/produk/presentation/state/DetailProdukState.kt
@@ -0,0 +1,12 @@
+package com.ptpws.ikikasir.feature.produk.presentation.state
+
+import com.ptpws.ikikasir.feature.produk.domain.model.Produk
+
+data class DetailProdukState(
+    val produk: Produk? = null,
+    val categoryName: String = "Umum",
+    val isLoading: Boolean = false,
+    val isDeleted: Boolean = false,
+    val errorMessage: String? = null,
+    val showDeleteDialog: Boolean = false
+)
