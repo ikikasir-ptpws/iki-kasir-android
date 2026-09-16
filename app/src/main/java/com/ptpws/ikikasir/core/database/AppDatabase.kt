d03b464d70b81e4e7f2173fb13238f4220255138
Ikhlas Sahputra <ikhlasamalillah568@gmail.com>
2026-09-16 11:15:42 +0700

    feat: update AppDatabase to include StockMovementEntity and StokAdjustmentDao

--- app/src/main/java/com/ptpws/ikikasir/core/database/AppDatabase.kt
+++ app/src/main/java/com/ptpws/ikikasir/core/database/AppDatabase.kt
@@ -5,21 +5,25 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.ptpws.ikikasir.feature.kategori.data.local.dao.KategoriDao
 import com.ptpws.ikikasir.feature.kategori.data.local.entity.KategoriEntity
+import com.ptpws.ikikasir.feature.manajemenstok.data.local.dao.StokAdjustmentDao
+import com.ptpws.ikikasir.feature.manajemenstok.data.local.entity.StockMovementEntity
 import com.ptpws.ikikasir.feature.produk.data.local.dao.ProdukDao
 import com.ptpws.ikikasir.feature.produk.data.local.entity.ProdukEntity
 
 @Database(
     entities = [
         KategoriEntity::class,
-        ProdukEntity::class
+        ProdukEntity::class,
+        StockMovementEntity::class
     ],
-    version = 8,
+    version = 10,
     exportSchema = false
 )
 @TypeConverters(TimestampConverter::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract val kategoriDao: KategoriDao
     abstract val produkDao: ProdukDao
+    abstract val stokAdjustmentDao: StokAdjustmentDao
 
     companion object {
         const val DATABASE_NAME = "ikikasir_db"
