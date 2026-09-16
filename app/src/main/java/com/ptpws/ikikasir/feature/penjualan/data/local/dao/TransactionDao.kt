e42a6addb0ffbc3b82af732d81b257640a96088d
Ikhlas Sahputra <ikhlasamalillah568@gmail.com>
2026-09-16 13:25:17 +0700

    feat: add TransactionDao interface for managing transaction data

---
+++ app/src/main/java/com/ptpws/ikikasir/feature/penjualan/data/local/dao/TransactionDao.kt
@@ -0,0 +1,26 @@
+package com.ptpws.ikikasir.feature.penjualan.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import com.ptpws.ikikasir.feature.penjualan.data.local.entity.TransactionEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TransactionDao {
+
+    @Query("SELECT * FROM transactions ORDER BY createdAt DESC")
+    fun getAllTransactionsFlow(): Flow<List<TransactionEntity>>
+
+    @Upsert
+    suspend fun insertOrUpdate(transaction: TransactionEntity)
+
+    @Upsert
+    suspend fun insertOrUpdateAll(transactions: List<TransactionEntity>)
+
+    @Query("SELECT * FROM transactions WHERE isSynced = 0")
+    suspend fun getUnsyncedTransactions(): List<TransactionEntity>
+
+    @Query("UPDATE transactions SET isSynced = 1 WHERE transactionId = :transactionId")
+    suspend fun markAsSynced(transactionId: String)
+}
