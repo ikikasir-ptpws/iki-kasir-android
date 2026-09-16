8db7cdb5db77ccc459b19f7934d86a404291b33f
Ikhlas Sahputra <ikhlasamalillah568@gmail.com>
2026-09-16 15:47:14 +0700

    feat: update DetailTransaksiActivity to pass transactionId to DetailTransaksiScreen

--- app/src/main/java/com/ptpws/ikikasir/screens/penjualan/DetailTransaksiActivity.kt
+++ app/src/main/java/com/ptpws/ikikasir/screens/penjualan/DetailTransaksiActivity.kt
@@ -4,24 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.ptpws.ikikasir.screens.penjualan.ui.theme.IKIKASIRTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailTransaksiActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val transactionId = intent.getStringExtra("TRANSACTION_ID") ?: ""
+
         setContent {
             IKIKASIRTheme {
-               DetailTransaksiScreen(onBack = {finish()})
+                DetailTransaksiScreen(
+                    transactionId = transactionId,
+                    onBack = { finish() }
+                )
             }
         }
     }
 }
-
