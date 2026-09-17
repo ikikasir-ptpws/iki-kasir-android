47b705cdcbee3fc70b019851634423f16ccc0093
Ikhlas Sahputra <ikhlasamalillah568@gmail.com>
2026-09-17 14:52:16 +0700

    feat: add terjualHariIni and total30HariTerakhir fields to DetailProdukState

--- app/src/main/java/com/ptpws/ikikasir/feature/produk/presentation/state/DetailProdukState.kt
+++ app/src/main/java/com/ptpws/ikikasir/feature/produk/presentation/state/DetailProdukState.kt
@@ -5,6 +5,8 @@ import com.ptpws.ikikasir.feature.produk.domain.model.Produk
 data class DetailProdukState(
     val produk: Produk? = null,
     val categoryName: String = "Umum",
+    val terjualHariIni: Int = 0,
+    val total30HariTerakhir: Int = 0,
     val isLoading: Boolean = false,
     val isDeleted: Boolean = false,
     val errorMessage: String? = null,
