80914f1eda83ab2cf5287cc2a3bf9b2629d6743e
Ikhlas Sahputra <ikhlasamalillah568@gmail.com>
2026-09-19 13:24:23 +0700

    feat: add bindings for QueueHistoryRemoteDataSource and QueueHistoryRepository in AntreanModule

--- app/src/main/java/com/ptpws/ikikasir/feature/antrean/di/AntreanModule.kt
+++ app/src/main/java/com/ptpws/ikikasir/feature/antrean/di/AntreanModule.kt
@@ -2,8 +2,12 @@ package com.ptpws.ikikasir.feature.antrean.di
 
 import com.ptpws.ikikasir.feature.antrean.data.remote.datasource.AntreanRemoteDataSource
 import com.ptpws.ikikasir.feature.antrean.data.remote.datasource.AntreanRemoteDataSourceImpl
+import com.ptpws.ikikasir.feature.antrean.data.remote.datasource.QueueHistoryRemoteDataSource
+import com.ptpws.ikikasir.feature.antrean.data.remote.datasource.QueueHistoryRemoteDataSourceImpl
 import com.ptpws.ikikasir.feature.antrean.data.repository.AntreanRepositoryImpl
+import com.ptpws.ikikasir.feature.antrean.data.repository.QueueHistoryRepositoryImpl
 import com.ptpws.ikikasir.feature.antrean.domain.repository.AntreanRepository
+import com.ptpws.ikikasir.feature.antrean.domain.repository.QueueHistoryRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -25,4 +29,16 @@ abstract class AntreanModule {
     abstract fun bindAntreanRepository(
         impl: AntreanRepositoryImpl
     ): AntreanRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindQueueHistoryRemoteDataSource(
+        impl: QueueHistoryRemoteDataSourceImpl
+    ): QueueHistoryRemoteDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindQueueHistoryRepository(
+        impl: QueueHistoryRepositoryImpl
+    ): QueueHistoryRepository
 }
