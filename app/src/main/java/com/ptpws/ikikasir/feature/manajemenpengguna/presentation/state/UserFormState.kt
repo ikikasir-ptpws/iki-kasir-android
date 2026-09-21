415a6412920a37bdc7dae836767a66ce871926d6
Ikhlas Sahputra <ikhlasamalillah568@gmail.com>
2026-09-21 11:13:02 +0700

    feat: add UserFormState data class for user form management

---
+++ app/src/main/java/com/ptpws/ikikasir/feature/manajemenpengguna/presentation/state/UserFormState.kt
@@ -0,0 +1,13 @@
+package com.ptpws.ikikasir.feature.manajemenpengguna.presentation.state
+
+data class UserFormState(
+    val id: String = "",
+    val fullName: String = "",
+    val email: String = "",
+    val password: String = "",
+    val roleId: String = "Kasir",
+    val isActive: Boolean = true,
+    val isLoading: Boolean = false,
+    val isSuccess: Boolean = false,
+    val error: String? = null
+)
