1aeada07b78fff007985c1c5f830853eee4d9675
Ikhlas Sahputra <ikhlasamalillah568@gmail.com>
2026-09-24 15:27:52 +0700

    feat: add tax-related fields to PembayaranState for improved transaction handling

--- app/src/main/java/com/ptpws/ikikasir/feature/penjualan/presentation/state/PembayaranState.kt
+++ app/src/main/java/com/ptpws/ikikasir/feature/penjualan/presentation/state/PembayaranState.kt
@@ -1,5 +1,6 @@
 package com.ptpws.ikikasir.feature.penjualan.presentation.state
 
+import com.ptpws.ikikasir.feature.pengaturan.domain.model.TaxSetting
 import com.ptpws.ikikasir.feature.penjualan.domain.model.CartItem
 import com.ptpws.ikikasir.feature.penjualan.domain.model.PenjualanTransaksi
 
@@ -9,6 +10,10 @@ data class PembayaranState(
     val subtotal: Double = 0.0,
     val totalItemCount: Int = 0,
     val totalPcsCount: Int = 0,
+    val taxSetting: TaxSetting = TaxSetting(),
+    val ppnAmount: Double = 0.0,
+    val grandTotal: Double = 0.0,
+    val ppnLabel: String = "",
     val metodePembayaran: String = "Tunai",
     val uangDiterimaText: String = "",
     val uangDiterima: Double = 0.0,
